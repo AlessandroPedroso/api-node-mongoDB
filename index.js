d6bd73579750ddb23245b002f6ab2fe0d851907a
AlessandroPedroso <alessandropedrosoti@gmail.com>
2022-12-16 12:20:50 -0300

    configuração do mongoose

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
 import { request, response } from "express";
 import express from 'express';
+import mongoose from "mongoose";
+import User from "./models/user.js";
 
 const app = express()
 app.use(express.json())
 
-const users = []
+app.get('/users', async (request, response) => {
 
-app.get('/users', (request,response) =>{
+    const users = await User.find()
 
-    return response.json(users)
+    return response.status(200).json(users)
 })
 
-app.post('/users', (request,response) =>{
+app.post('/users', async (request, response) => {
+
+    const user = request.body
+
+    const newUser = await User.create(user)
+
+    return response.status(201).json(newUser)
+})
+
+
+mongoose.connect("mongodb+srv://alessandro:93oY3G5lAVdtZ5Bx@cluster0.7uinhdl.mongodb.net/?retryWrites=true&w=majority")
+    .then(() => console.log("Banco de dados conectado!"))
+    .catch(() => console.log("Não conectou"))
     
-    // const name = request.body.name
-    // const age = request.body.age
-
-    const {name,age} = request.body
-
-    users.push({name,age})
-
-    // console.log(request)
-    // console.log(request.body)
-
-    return response.json({name,age})
-})
-
-
-
 app.listen(3000)
